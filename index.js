85ac351dc1116492ddc958f051b2af43d51c1045
Anmol mishra <mishraanmol534gmail.com>
2024-05-07 13:31:23 +0530

    commit

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 import express from "express";
-//import cors from "cors";
 import ticketRouter from "./routers/ticketRouter.js";
 import sequelize from "./database/db.js";
 
 const app = express();
-//dotenv.config({ path: "./config/config.env" });
-
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
